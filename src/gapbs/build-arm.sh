e98ff3e974ce672bb887253b85bd40fde059ea96
Harshil Patel <harshilp2107@gmail.com>
2024-09-27 13:37:53 -0700

    Update src/gapbs/build-arm.sh
    Co-authored-by: Erin (Jianghua) Le <jianghuale@gmail.com>

--- src/gapbs/build-arm.sh
+++ src/gapbs/build-arm.sh
@@ -6,5 +6,16 @@ if [ ! -f ./packer ]; then
     rm packer_${PACKER_VERSION}_linux_arm64.zip;
 fi
 
+# make the flash0.sh file
+mkdir files
+cd ./files
+
+dd if=/dev/zero of=flash0.img bs=1M count=64
+dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=flash0.img conv=notrunc
+cd ..
+
+wget https://storage.googleapis.com/dist.gem5.org/dist/develop/images/arm/ubuntu-24-04/arm-ubuntu-24.04-20240823.gz
+gunzip arm-ubuntu-24.04-20240823.gz 
+
 ./packer init arm-ubuntu-24-04-gapbs.pkr.hcl
 ./packer build arm-ubuntu-24-04-gapbs.pkr.hcl
